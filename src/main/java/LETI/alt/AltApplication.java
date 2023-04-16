9d209b1d89ca66e7e4f2a0eb31384f82f3bdcfe8
satancucumber <amatomato@yandex.ru>
2023-04-16 13:17:17 +0300

    Initial commit

---
+++ src/main/java/LETI/alt/AltApplication.java
@@ -0,0 +1,13 @@
+package LETI.alt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AltApplication.class, args);
+	}
+
+}
---
+++ src/test/java/LETI/alt/AltApplicationTests.java
@@ -0,0 +1,13 @@
+package LETI.alt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AltApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
